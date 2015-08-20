b0059e7987bff765956ee4bb381947f2f54d0903
Jorge Hoya <jorge@nosoynadie.net>
2015-08-20 16:23:07 +0200

    more...

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -10,7 +10,7 @@ function autoload($className)
         $className = substr($className, $lastNsPos + 1);
         $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
     }
-    echo $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
 
     require_once ($fileName);
 }
