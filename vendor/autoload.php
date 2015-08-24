7c0d681f7aa5da8c353661e4f676443d53fb0cef
Jorge Hoya <jorge@nosoynadie.net>
2015-08-24 13:35:47 +0200

    more

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -11,6 +11,7 @@ function autoload($className)
         $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
     }
     $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+	echo 'Fichero a buscar: ' . $fileName;
 
     require_once ($fileName);
 }
