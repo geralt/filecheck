b38ba91b62e38c170a950301ec867f723805db72
Jorge Hoya <jorge@nosoynadie.net>
2015-08-24 16:49:08 +0200

    test working

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -3,7 +3,6 @@
 define ('BASEDIR', __DIR__);
 
 spl_autoload_register( function($className){
-    echo $className . PHP_EOL;
     if (substr($className, 0, 9) !== 'FileCheck') {return;}
     $className = ltrim($className, '\\');
     $fileName  = '' . BASEDIR . DIRECTORY_SEPARATOR;
@@ -14,9 +13,8 @@ spl_autoload_register( function($className){
         $fileName  .= str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
     }
     $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-    //$fileName .= str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
-	echo 'Fichero a buscar: ' . $fileName . ' para la clase: ' . $className;
-    echo PHP_EOL . 'existe el fichero? ' . (is_file($fileName));
+	//echo 'Fichero a buscar: ' . $fileName . ' para la clase: ' . $className;
+    //echo PHP_EOL . 'existe el fichero? ' . (is_file($fileName));
     require_once ($fileName);
 });
 
