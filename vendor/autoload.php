da73883044f0e19d436e0a2b3a0035f61239dcdb
Jorge Hoya <jorge@nosoynadie.net>
2015-08-24 15:03:12 +0200

    r

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -1,15 +1,15 @@
 <?php
 /**/
-define ('BASEDIR', dirname(__FILE__));
+define ('BASEDIR', __DIR__));
 
 spl_autoload_register( function($className){
     $className = ltrim($className, '\\');
-    $fileName  = '' . BASEDIR . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR;
+    $fileName  = '' . BASEDIR . DIRECTORY_SEPARATOR;
     $namespace = '';
     if ($lastNsPos = strrpos($className, '\\')) {
         $namespace = substr($className, 0, $lastNsPos);
         $className = substr($className, $lastNsPos + 1);
-        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+        $fileName  .= str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
     }
     $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
 	echo 'Fichero a buscar: ' . $fileName . ' para la clase: ' . $className;
