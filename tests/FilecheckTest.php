733ccec31f90cab8bb4212ab659582446cd2172d
Jorge Hoya <jorge@nosoynadie.net>
2015-08-24 13:58:13 +0200

    more

--- tests/FilecheckTest.php
+++ tests/FilecheckTest.php
@@ -3,7 +3,7 @@
 //namespace FileCheck;
 
 //use FileCheck;
-include_once('../vendor/autoload.php');
+include_once ( 'vendor/autoload.php');
 
 class FilecheckTest extends \PHPUnit_Framework_TestCase 
 {
